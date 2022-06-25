e0eff74998252da78e37040a78fa71a24a79b2fc
ihsan <ihsan@pop-os.localdomain>
2022-06-25 23:37:15 +0700

    finish ben awad react typescript tutorial

---
+++ src/TextField.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useRef } from "react";
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+interface Props {
+    text: string;
+    ok?: boolean;
+    i?: number;
+    fn?: (bob: string) => string;
+    person: Person;
+    handleChange: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+export const TextField: React.FC<Props> = ({ handleChange }) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const divRef = useRef<HTMLDivElement>(null);
+
+    return (
+        <div ref={divRef}>
+            <input ref={inputRef} onChange={handleChange} />
+        </div>
+    );
+};
